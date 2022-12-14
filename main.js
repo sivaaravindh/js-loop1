86980e2c11ce10d1bc36002c3e9d56f092b000bb
sivaaravindh <sivaaravind262003@gmail.com>
2022-12-14 08:18:13 -0800

    index

---
+++ main.js
@@ -0,0 +1,10 @@
+let num=parseInt (prompt("Enter the number"));
+
+for (let i=num; i>0; i--){
+        
+    for(let j=1; j<=i; j++){
+    
+        document.write(j)
+    }
+    document.write("<br>")
+}
